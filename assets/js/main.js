b24605f1b6519234fe9bf5672be61ae102ba3fde
Surfskills <ojjfred@gmail.com>
2023-10-17 22:28:02 +0000

    Added the Surskills web page into the files of my portfolio project. The copyright link will open the page on a new tab

--- assets/js/main.js
+++ assets/js/main.js
@@ -260,3 +260,25 @@
   new PureCounter();
 
 })()
+
+// Surskills
+
+// Add your custom JavaScript here
+
+// Example JavaScript for a smooth scroll when clicking navbar links
+$(document).ready(function() {
+  $(".navbar a, .nav-item a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+// Add your custom JavaScript for other interactions
+
